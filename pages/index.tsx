e028bd763242a255122ac8301071ccc5625c7008
OluC94 <coker.olu94@gmail.com>
2023-01-16 12:08:37 +0000

    set global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,5 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <h1 className="header-class">Hello World</h1>;
 }
